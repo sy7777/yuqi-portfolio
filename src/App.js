725be7ddc81a784401f8f1c597a54ef142821ce6
Yuqi Sui <yuqi.sui@tpgtelecom.com.au>
2025-07-11 16:35:58 +0800

    structure built

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import FrontPage from './components/FrontPage';
+import Menu from './components/Menu';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <FrontPage />
+      <Menu />
+      <Content />
+      <Footer />
     </div>
   );
 }
